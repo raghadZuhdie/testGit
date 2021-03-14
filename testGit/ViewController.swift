1c806dbe953f92250be87f00826196f8b1ed5aab
Tariq Al-aqrabawi <aqrabawit@icloud.com>
2021-03-14 06:26:29 -0700

    newFunction

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
-jhhjhjjh
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        //comments
     }
 
 
